0a80a2605ebb1126b12a2cb70b4fd20957af06f3
Murilo <19322082+muriloloffi@users.noreply.github.com>
2024-02-19 10:15:02 -0300

    home "welcome" component

---
+++ src/templates/Home.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { storeToRefs } from 'pinia';
+import { useAuthStore } from '@/stores';
+const authStore = useAuthStore();
+const { user } = storeToRefs(authStore);
+</script>
+
+<template>
+  <div v-if="user">
+    <h1>Hi, {{ user.name }}!</h1>
+    <p>Welcome to the Gym Passport!</p>
+    <p><router-link to="/users">Users view</router-link></p>
+  </div>
+</template>
