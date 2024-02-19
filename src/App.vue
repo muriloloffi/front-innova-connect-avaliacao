1680cdf775b900e74ff52b75d67a8c3966cb95f3
Murilo <19322082+muriloloffi@users.noreply.github.com>
2024-02-19 04:11:13 -0300

    wip registration form - email validation

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,36 @@
-<script setup>
-
-</script>
-
 <template>
-  <h1 class="text-3xl font-bold underline">Gym Passport</h1>
+  <div id="app">
+    <Form @submit="onSubmit">
+      <Field type="text" name="name"/>
+      <Field type="email" name="email" :rules="validateEmail"/>
+      <ErrorMessage name="email"/>
+      <Field type="password" name="password"/>
+
+      <button type="submit">Submit</button>
+    </Form>
+  </div>
 </template>
+
+<script setup>
+import { Form, Field, ErrorMessage } from 'vee-validate';
+
+function onSubmit(values) {
+  console.log(JSON.stringify(values, null, 2));
+}
+
+function validateEmail(value) {
+  if (!value) {
+    return 'This field is required';
+  }
+
+  const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+  if (!regex.test(value)) {
+    return 'Invalid email';
+  }
+
+  return true;
+}
+</script>
 
 <style scoped>
 </style>
