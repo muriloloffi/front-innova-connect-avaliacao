45b1075142cb8851f8d812b16e827e2699cf28be
Murilo <19322082+muriloloffi@users.noreply.github.com>
2024-02-19 05:05:31 -0300

    create validation schema with yup

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,33 @@
+<script setup>
+import { Form, Field, ErrorMessage } from 'vee-validate';
+import * as yup from 'yup';
+
+const schema = yup.object({
+  name: yup.string().required(),
+  email: yup.string().email().required(),
+  password: yup.string().required(),
+});
+
+function onSubmit(values) {
+  console.log(JSON.stringify(values, null, 2));
+}
+</script>
+
 <template>
   <div id="app">
-    <Form @submit="onSubmit">
-      <Field type="text" name="name"/>
-      <Field type="email" name="email" :rules="validateEmail"/>
+    <Form class="flex columns-auto" :validation-schema="schema" @submit="onSubmit">
+      <Field type="email" name="email"/>
       <ErrorMessage name="email"/>
+      <Field type="text" name="name"/>
+      <ErrorMessage name="name"/>
       <Field type="password" name="password"/>
+      <ErrorMessage name="password"/>
 
       <button type="submit">Submit</button>
     </Form>
   </div>
 </template>
 
-<script setup>
-import { Form, Field, ErrorMessage } from 'vee-validate';
-
-function onSubmit(values) {
-  console.log(JSON.stringify(values, null, 2));
-}
-
-function validateEmail(value) {
-  if (!value) {
-    return 'This field is required';
-  }
-
-  const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
-  if (!regex.test(value)) {
-    return 'Invalid email';
-  }
-
-  return true;
-}
-</script>
 
 <style scoped>
 </style>
