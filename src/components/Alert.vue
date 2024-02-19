042463c949e7d810a9c5e4b257495ee0a8543a8a
Murilo <19322082+muriloloffi@users.noreply.github.com>
2024-02-19 09:16:07 -0300

    alert component

---
+++ src/components/Alert.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { storeToRefs } from 'pinia';
+import { useAlertStore } from '@/stores';
+
+const alertStore = useAlertStore();
+const { alert } = storeToRefs(alertStore);
+</script>
+
+<template>
+  <div v-if="alert" class="container mx-auto sm:px-4">
+    <div class="m-6">
+      <div class="relative px-3 py-3 mb-4 border rounded" :class="alert.type"> 
+        <button @click="alertStore.clear()" class="inline-block align-middle text-center select-none border font-normal whitespace-no-wrap rounded px-3 leading-normal no-underline text-blue-700 bg-transparent absolute top-0 bottom-0 right-0 py-3">×</button>
+        {{ alert.message }}
+      </div>
+    </div>
+  </div>
+</template>
