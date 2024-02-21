e85945449705dea4c0869bd23ad336b34dd27aef
Murilo <19322082+muriloloffi@users.noreply.github.com>
2024-02-21 19:57:44 -0300

    Refactor gym list layout

--- src/templates/gyms/List.vue
+++ src/templates/gyms/List.vue
@@ -11,35 +11,37 @@
 </script>
 
 <template>
-  <h1>Gyms</h1>
-  <table class="w-full max-w-full mb-4 bg-transparent table-auto">
-    <thead class="table-header-group">
-      <tr>
-        <th class="border">Name</th>
-        <th>Description</th>
-        <th>Phone</th>
-        <th>Location</th>
-        <th>Actions</th>
-      </tr>
-    </thead>
-    <tbody>
-      <template v-if="gyms.length">
-        <tr v-for="gym in gyms" :key="gym.id">
-          <td class="table-cell">{{ gym.name }}</td>
-          <td>{{ gym.description }}</td>
-          <td>{{ gym.phone }}</td>
-          <td>{{ gym.coordinates }}</td>
-          <td style="white-space: nowrap; display: flex; justify-content: center;">
-            <router-link :to="`/gym/update/${gym.id}`" class="flex align-middle text-center select-none border font-normal whitespace-no-wrap rounded px-3 leading-normal no-underline py-1 text-xs bg-blue-600 text-white hover:bg-blue-400 mr-1">Edit</router-link>
-          </td>
+  <h1 class="text-3xl mb-4">Gyms</h1>
+  <div class="shadow rounded border-b border-gray-200">
+    <table class="w-full max-w-full mb-4 bg-transparent table-auto">
+      <thead class="bg-gray-600 text-white">
+        <tr>
+          <th class="text-left">Name</th>
+          <th class="text-left">Description</th>
+          <th class="text-left">Phone</th>
+          <th class="text-left">Location</th>
+          <th class="text-center">Actions</th>
         </tr>
-      </template>
-      <tr v-if="gyms.loading">
-        <td colspan="4" class="text-center">Loading...</td>
-      </tr>
-      <tr v-if="gyms.error">
-        <td colspan="4" class="text-center">{{ gyms.error }}</td>
-      </tr>
-    </tbody>      
-  </table>
+      </thead>
+      <tbody>
+        <template v-if="gyms.length">
+          <tr v-for="gym in gyms" class="odd:bg-white" :key="gym.id">
+            <td>{{ gym.name }}</td>
+            <td>{{ gym.description }}</td>
+            <td>{{ gym.phone }}</td>
+            <td>{{ gym.coordinates }}</td>
+            <td class="whitespace-nowrap flex justify-center">
+              <router-link :to="`/gym/update/${gym.id}`" class="flex justify-center align-middle text-center select-none border font-normal whitespace-no-wrap rounded px-3 leading-normal no-underline py-1 text-xs bg-blue-600 text-white hover:bg-blue-400">Edit</router-link>
+            </td>
+          </tr>
+        </template>
+        <tr v-if="gyms.loading">
+          <td colspan="4" class="text-center">Loading...</td>
+        </tr>
+        <tr v-if="gyms.error">
+          <td colspan="4" class="text-center">{{ gyms.error }}</td>
+        </tr>
+      </tbody>      
+    </table>
+  </div>
 </template>
